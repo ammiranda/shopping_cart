9c88cabeccdd90af69fcc512b8e653e01986a960
Alexander <alexandermichaelmiranda@gmail.com>
2013-06-20 21:43:29 -0400

    Jquery updates

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
 /* Input field appending list */
 
 $(document).ready(function(){
-	$('input[type="submit"]').click(function () { 
+	$('#submit').click(function(e) { 
+		e.preventDefault();
 		var item = $('#itemName').val();
-		$('#theList').append('<li>'item'</li>'); 
 		console.log(item);
+		$('#theList').append('<li class="item"><input type="checkbox" class="check"><span class="item2">'+ item +'</span></li>'); 
+		$('#itemName').val("");
 				});
+	$('input.check').change(function(){
+		$('.item2').siblings().toggleClass('strike');
+	});
 	
+	/* Enables Sorting of li's */
 	
-	
+	$('#theList').sortable({
+		}).disableSelection();
+		
+	/* Removes Checked Items */
+		
+	$('#remove').submit(function(event) {
+		event.preventDefault();
+		$('#theList li').each(function () {
+			if($(this).find('.item2').hasClass('strike')){
+				$(this).remove();
+				}
+		
+		});
+		});
 	});
